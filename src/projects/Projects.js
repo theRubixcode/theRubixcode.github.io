a57aee02c24b75f004e3c73aa063c3a577b61d0b
theRubixcode <rubixcode.india@gmail.com>
2023-08-24 15:29:13 +0530

    projects added

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ProjectsMenu from "./ProjectsMenu";
 
 export default class Projects extends Component {
   render() {
     return (
-      <div>Projects</div>
-    )
+      <>
+        <ProjectsMenu />
+      </>
+    );
   }
 }

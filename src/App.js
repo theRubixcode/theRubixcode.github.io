85237ca349c86d3e9ea4335ea796531fb08c445d
theRubixcode <rubixcode.india@gmail.com>
2023-08-24 14:13:51 +0530

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./nav/Nav.js";
+import About from "./about/About.js";
+import Skills from "./skills/Skills.js";
+import Projects from "./projects/Projects.js";
+import Contact from "./contact/Contact.js";
+import "./styles/app.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={About} />
+        <Route path="/" element={Skills} />
+        <Route path="/" element={Projects} />
+        <Route path="/" element={Contact} />
+      </Routes>
+    </Router>
   );
 }
 

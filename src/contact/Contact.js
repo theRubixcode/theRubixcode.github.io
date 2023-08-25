0e3affd47c0c61b9b500feeaa1affc1d26e485e1
theRubixcode <rubixcode.india@gmail.com>
2023-08-25 18:28:25 +0530

    contact style udpated

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactMenu from "./ContactMenu";
+import Avatar from "../avatar/Avatar";
 import "../styles/contact.css";
 
 const Contact = () => {
   return (
     <>
+      <Avatar page="contact" />
       <ContactMenu />
     </>
   );
